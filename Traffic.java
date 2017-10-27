c7dd3ae3bc8d10ea61672b8927ba906f8a6c04f5
Nikolay Eremeev <nikolay.eremeev@rwth-aachen.de>
2017-10-27 18:15:19 +0200

    task 1 completed

--- Traffic.java
+++ Traffic.java
@@ -1,5 +1,19 @@
 public class Traffic {
-    public static void main(String[] args) {
-        int a = 2;
-    }
+
+	public static double timeGlobal = 0.0;	
+
+	public static void main(String[] args) {
+	
+		Vehicle auto1 = new Vehicle("BMW", 260.5);
+		Vehicle auto2 = new Vehicle("Audi", 200.5);
+		Vehicle auto3 = new Vehicle("Toyota", 360.5);
+		Vehicle auto4 = new Vehicle("UAZ", 460.5);
+		Vehicle auto5 = new Vehicle("GAZ", 60.5);
+	
+		for(; timeGlobal < 15; timeGlobal += 0.01){
+			Vehicle.printOut();
+			Vehicle.processing();
+		
+		}
+	}
 }
---
+++ Vehicle.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.text.DecimalFormat;
+
+public class Vehicle {
+	private String name;
+	private static int idMax = 1;
+	private int id;
+	private double speedMax;
+
+	private double mileage;
+	private double timeTotal;
+	private double timeCurrent;
+	private static ArrayList<Vehicle> list = new ArrayList<Vehicle>();
+	public Vehicle() {
+		name = "";
+		speedMax = 0.0;	
+	}
+	
+	public Vehicle(String sName, double dSpeedMax) {
+		name = sName;
+		speedMax = dSpeedMax;
+		initialize();
+	}
+
+	private void initialize() {
+		id = idMax;
+		idMax = idMax + 1;
+        	timeTotal = 0.0;
+        	timeCurrent = 0.0;
+		mileage = 0.0;
+		list.add(this);
+	}
+	
+	public static void printOut() {
+
+		DecimalFormat df = new DecimalFormat("#.##");
+
+		System.out.println("\nID\tName\t\tMax Speed\tMileage\t\t\tTotal Time\tCurrent Time");
+		String plus = "++++++++++++++++++++++++++++";
+		System.out.println(plus + plus + plus);
+		for (Vehicle veh : list) System.out.println(veh.id + "\t" + veh.name + "\t\t"
+			+ df.format(veh.speedMax) + "\t\t" + df.format(veh.mileage) + "\t\t\t" + df.format(veh.timeTotal) + "\t\t" 
+			+ df.format(veh.timeCurrent));
+	}
+
+	public static void processing() {
+		for (Vehicle veh : list) veh.mileage = Traffic.timeGlobal * veh.speedMax;
+	} 
+
+}
